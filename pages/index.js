c0cdc790d1a09b756e56750f874e643f86fd39ac
rameshkorapati03 <134677081+rameshkorapati03@users.noreply.github.com>
2023-08-04 20:13:10 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Welcome to Sree Srinivasa</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
