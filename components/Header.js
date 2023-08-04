520dce666c3b629fc4a4736a60ce7e3b2861a1d7
rameshkorapati03 <134677081+rameshkorapati03@users.noreply.github.com>
2023-08-04 20:28:28 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,28 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
-}
+import React from 'react';
+import { Layout, Space } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
+const headerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: '#7dbcea',
+};
+const HeaderSite = () => (
+  <Space
+    direction="vertical"
+    style={{
+      width: '100%',
+    }}
+    size={[0, 48]}
+  >
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Content style={contentStyle}>Content</Content>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>    
+    </Layout>
+  </Space>
+);
+export default HeaderSite;
